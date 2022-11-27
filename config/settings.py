a82dcbce0147436e4e641a369297e95d4be373e2
rlJzr <eopy0928@naver.com>
2022-11-27 22:31:38 +0900

    allow host 추가

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x8k*w%7_kkm)=hc3e=@rar-jpioq(!&zi&1#93oqb@h^vm9s1+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://rljzr.pythonanywhere.com/']
 
 
 # Application definition
